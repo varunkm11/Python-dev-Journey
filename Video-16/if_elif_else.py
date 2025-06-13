b957f46d1f42c1037f22e47061e7501e02305685
varunkm11 <varunkumarsingh818@gmail.com>
2025-06-13 22:35:30 +0530

    Learning Python

---
+++ Video-16/if_elif_else.py
@@ -0,0 +1,7 @@
+x=7
+if x>10:
+    print("x is greater than 10")
+elif x==7:
+    print("x is equal to 7")
+else:
+    print("x is less than 10")
---
+++ Video-16/if_else.py
@@ -0,0 +1,6 @@
+x=3
+if x>5:
+    print("X is greater than 5 ")
+else:
+    print("X is not greater than 5.")
+    
