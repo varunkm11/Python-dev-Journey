3554bc8caf0dd27faa4f993f6b1cdc9745b6c9a7
varunkm11 <varunkumarsingh818@gmail.com>
2025-06-17 23:18:45 +0530

    Learning Python

---
+++ Video-32/tuples.py
@@ -0,0 +1,12 @@
+my_tuple=(1, 2, 3, 4, 5)
+mixed_tuple=(1, "Hello", 3.14, True)
+single_element_tuple=(42,)
+print(my_tuple[2])  # Accessing the third element
+
+# tuples are immutable
+#tuple methods
+# tuple methods
+print(len(my_tuple))  # Length of the tuple
+tuple_concatenation = my_tuple + mixed_tuple  # Concatenating tuples
+list=[1,2,3,4,5]
+print(tuple(list))  # Converting a list to a tuple
