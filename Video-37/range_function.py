1d36f1a86ef2ccd5888c33ac6080da15cb7c7023
varunkm11 <varunkumarsingh818@gmail.com>
2025-06-18 23:11:44 +0530

    Learning Python

---
+++ Video-37/range_function.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    print(i)
+# This code prints numbers from 0 to 9 using the range function.
+for i in range (2,6):
+    print(i)# This code prints numbers from 2 to 5 using the range function with a start and end value.
+for i in range(1, 10, 2):
+    print(i)  # This code prints odd numbers from 1 to 9 using the range function with a step value.
+for i in range(10,0,-2):
+    print(i)  # This code prints even numbers from 10 to 2 in reverse order using the range function with a negative step value.
